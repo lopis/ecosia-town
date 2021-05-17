7ade6c175324907db8a08b5f8be64077666de0a9
Joao Lopes <lopis@users.noreply.github.com>
2021-05-17 10:33:22 +0200

    Add kitchen

---
+++ tilesets/walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="walls" tilewidth="32" tileheight="32" tilecount="1750" columns="25">
+ <image source="../textures/walls.png" trans="ff00ff" width="800" height="2240"/>
+</tileset>
